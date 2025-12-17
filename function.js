903c14dd1d9e70ad3bbee75a40c559487311b5ff
Md. Anwar Hossain <aymankajolanwar@gmail.com>
2025-12-18 03:56:34 +0600

    this is a update

--- function.js
+++ function.js
@@ -1,7 +1,21 @@
-// heart functionality start
+const transctionData =[];
 
+// function for get inner text
+function getInnerText(id){
+    const innerTextValue = document.getElementById(id).innerText;
+    return innerTextValue;
+}
+
+// function for get inner text with conversion of number
+function getNumberInnerText(id){
+    const innerTextId = parseInt(document.getElementById(id).innerText);
+    return innerTextId;
+}
+
+
+// heart functionality start
 const hearts = document.getElementsByClassName('heart');
-let availableHeart = parseInt(document.getElementById('available-heart').innerText);
+let availableHeart = getNumberInnerText('available-heart');
 
 for(const heart of hearts){
     heart.addEventListener('click', function(){
@@ -9,5 +23,36 @@ for(const heart of hearts){
         document.getElementById('available-heart').innerText = availableHeart;
     })
 }
-
 // heart functionality end
+
+
+// call functionality start
+
+    // Emergency Button start
+document.getElementById('emergency-btn')
+    .addEventListener('click', function(){
+        const availableCoin = parseInt(document.getElementById('available-coin').innerText);
+
+        const serviceTitle = getInnerText('national-Emaergency');
+        const serviceNumber = getInnerText('service-number');
+
+        if(19 < availableCoin){
+            alert('you are calling in '+ serviceTitle+' at '+serviceNumber);
+            const currentCoin = availableCoin-20;
+            document.getElementById('available-coin').innerText = currentCoin;
+        }else{
+            alert("you don't have the suffecient Coin to call in " + serviceTitle+' at '+serviceNumber);
+            return;
+        }
+
+        const serviceInformation ={
+            service:'National Emergency',
+            hotlineNum: 999,
+            time: new Date().toLocaleTimeString()
+        }
+
+        transctionData.push(serviceInformation);
+        console.log(transctionData);
+    })
+
+// call functionality end
